e59582d9930893fcf8889824b646317333f8ee6e
ZhelinCheng <chengzhelin@foxmail.com>
2020-07-24 16:48:26 +0800

    更新

---
+++ 1025.除数博弈.js
@@ -0,0 +1,25 @@
+/*
+ * @Author       : Zhelin Cheng
+ * @Date         : 2020-07-24 15:14:18
+ * @LastEditors  : Zhelin Cheng
+ * @LastEditTime : 2020-07-24 16:48:22
+ * @FilePath     : /leetcode/1025.除数博弈.js
+ * @Description  : 未添加文件描述
+ */ 
+/*
+ * @lc app=leetcode.cn id=1025 lang=javascript
+ *
+ * [1025] 除数博弈
+ */
+
+// @lc code=start
+/**
+ * @param {number} N
+ * @return {boolean}
+ */
+
+var divisorGame = function(N) {
+    return N % 2 === 0
+};
+// @lc code=end
+
